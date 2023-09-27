b97fb5444925488917b8be13edf566bb2153755e
djaus2 <davidjones@sportronics.com.au>
2023-09-27 14:14:15 +1000

    rAdded SerialPort_Echo project

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Extended demo projects for Meadow ProjectLab.
 - [Meadow.Core.Samples](https://github.com/WildernessLabs/Meadow.Core.Samples)
 - [Meadow.Foundation.Grove](https://github.com/WildernessLabs/Meadow.Foundation.Grove)
 
+> Nb: Currently a .netstandard2.1 projects
+
 ## WiFi Demo
 
 This project is based upon the [Meadow ProjectLab_Demo](https://github.com/WildernessLabs/Meadow.ProjectLab/tree/main/Source/). It needs the project **Meadow.ProjectLab** located with that project. _(You need to clone that GitHub project and then add the Meadow.ProjectLab project as a project reference to this project.)_ 
@@ -20,8 +22,13 @@ This project is based upon the [Meadow ProjectLab_Demo](https://github.com/Wilde
 The WiFi functionality is based upon the [Meadow.Core.Samples WiFi_Basics](https://github.com/WildernessLabs/Meadow.Core.Samples/tree/main/Source/Network/WiFi_Basics/CS) project. 
 > This project demonstrates using the display in a reusable manner, as a menu. The user selects a network from the locally found WiFi access poinst and then selects from a given list of passwords.
 
+## SerialPort_Echo
 
-> Nb: Currently a .netstandard2.1 project
+Demonstrates loopback serial transmission.  
+- Uses COM1. 
+- Jumper COM1 Tx to Rx in mikro BUS socket 1 _(Not socket2)_
+- Adapdted from **Meadow.Core.Samples/IO/SerialPort_Echo project**_
+
 
 ### Further Links
 - [UeberDaniel/Meadow-Ws2812Display-Driver](https://github.com/UeberDaniel/Meadow-Ws2812Display-Driver)
---
+++ SerialPort_Echo/MeadowApp.cs
@@ -0,0 +1,66 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPort_Echo
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        ISerialPort port;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Available serial ports:");
+            foreach (var name in Device.PlatformOS.GetSerialPortNames())
+            {
+                Resolver.Log.Info($"  {name.FriendlyName}");
+            }
+            var serialPortName = Device.PlatformOS.GetSerialPortName("COM1");
+            Resolver.Log.Info($"Using {serialPortName.FriendlyName}...");
+            port = Device.CreateSerialPort(serialPortName, 115200);
+            Resolver.Log.Info("\tCreated");
+            port.Open();
+            if (port.IsOpen)
+            {
+                Resolver.Log.Info("\tOpened");
+            }
+            else
+            {
+                Resolver.Log.Info("\tFailed to Open");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                string msgOut = "Hello Meadow!";
+                Resolver.Log.Info($"Writing data: \"{msgOut}\"");
+                port.Write(System.Text.Encoding.ASCII.GetBytes(msgOut));
+                port.Write(new byte[] { 0 }); //Null terminate string
+                var dataLength = port.BytesToRead;
+                var read = port.Read(buffer, 0, dataLength);
+                if (read == 0)
+                {
+                    Resolver.Log.Info($"Read {read} bytes");
+                }
+                else
+                {
+                    Resolver.Log.Info($"Read {read} bytes: {BitConverter.ToString(buffer, 0, read)}");
+                    string msgIn = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                    Resolver.Log.Info($"Read {read} bytes. Received message: \"{msgIn}");
+                    Resolver.Log.Info("\"");
+                }
+
+                await Task.Delay(2000);
+            }
+        }
+    }
+}
